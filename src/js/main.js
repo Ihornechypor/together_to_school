eb74bac47d252efec3bec9e419359f8f5fb87a91
ihor.nechypor <ihor.nechypor@plej.pl>
2022-05-06 11:24:04 +0200

    add oll worrds

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,24 @@ window.addEventListener("load", () => {
       sSearch: "Znajdź słowo / Знайди слово"
     }
   });
+  $(".js-dtTableAll").DataTable({
+    bPaginate: false,
+    bLengthChange: false,
+    bFilter: true,
+    ordering: false,
+    bInfo: false,
+    bAutoWidth: false,
+    oLanguage: {
+      sSearch: "Wpisz słowo / Впишіть слово"
+    }
+  });
+
+  const allWordsTable = document.querySelector(".js-dtTableAll");
+  const allWordsInput = document.querySelector(".js-table-all input");
+
+  allWordsInput.addEventListener("input", (e) => {
+    e.target.value === ""
+      ? allWordsTable.classList.add("table-hidden-empty")
+      : allWordsTable.classList.remove("table-hidden-empty");
+  });
 });
